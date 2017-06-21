aada1d46bae8f86af99b3da81cf5e020440dd545
Rubén Calvo <rubencm@gmail.com>
2017-06-21 22:07:09 +0200

    [feature/storage] Update local adapter test

--- tests/storage/adapter/local_test.php
+++ tests/storage/adapter/local_test.php
@@ -29,78 +29,60 @@
 
 	public function test_put_contents()
 	{
-		$this->adapter->put_contents('file.txt', '');
-		$this->assertTrue($this->adapter->exists('file.txt'));
-		$this->adapter->delete('file.txt');
+		$this->adapter->put_contents('file.txt', 'abc');
+		$this->assertTrue(file_exists('file.txt'));
+		$this->assertEquals(file_get_contents('file.txt'), 'abc');
+		unlink('file.txt');
 	}
 
 	public function test_get_contents()
 	{
-		$this->adapter->put_contents('file.txt', 'abc');
+		file_put_contents('file.txt', 'abc');
 		$this->assertEquals($this->adapter->get_contents('file.txt'), 'abc');
-		$this->adapter->delete('file.txt');
+		unlink('file.txt');
 	}
 
 	public function test_exists()
 	{
 		// Exists with files
-		$this->adapter->put_contents('file.txt', '');
-		$this->assertTrue($this->adapter->exists('file.txt'));
-		$this->adapter->delete('file.txt');
+		$this->assertTrue($this->adapter->exists('local_test.php'));
+		$this->assertFalse($this->adapter->exists('local_test2.php'));
 		// exists with directory
-		$this->adapter->create_dir('directory');
-		$this->assertTrue($this->adapter->exists('directory'));
-		$this->adapter->delete_dir('directory');
+		$this->assertTrue($this->adapter->exists('../adapter'));
+		$this->assertFalse($this->adapter->exists('../adapter2'));
 	}
 
 	public function test_delete()
 	{
-		$this->adapter->put_contents('file.txt', '');
-		$this->assertTrue($this->adapter->exists('file.txt'));
+		// Delete with files
+		file_put_contents('file.txt', '');
+		$this->assertTrue(file_exists('file.txt'));
 		$this->adapter->delete('file.txt');
-		$this->assertFalse($this->adapter->exists('file.txt'));
-
+		$this->assertFalse(file_exists('file.txt'));
+		// Delete with directories
+		mkdir('path/to/dir', 0777, true);
+		$this->assertTrue(file_exists('path/to/dir'));
+		$this->adapter->delete('path');
+		$this->assertFalse(file_exists('path/to/dir'));
 	}
 
 	public function test_rename()
 	{
-		$this->adapter->put_contents('file.txt', '');
-		$this->assertTrue($this->adapter->exists('file.txt'));
+		file_put_contents('file.txt', '');
 		$this->adapter->rename('file.txt', 'file2.txt');
-		$this->assertFalse($this->adapter->exists('file.txt'));
-		$this->assertTrue($this->adapter->exists('file2.txt'));
-		$this->adapter->delete('file2.txt');
+		$this->assertFalse(file_exists('file.txt'));
+		$this->assertTrue(file_exists('file2.txt'));
+		unlink('file2.txt');
 	}
 
 	public function test_copy()
 	{
-		$this->adapter->put_contents('file.txt', 'abc');
-		$this->assertTrue($this->adapter->exists('file.txt'));
+		file_put_contents('file.txt', 'abc');
 		$this->adapter->copy('file.txt', 'file2.txt');
-		$this->assertTrue($this->adapter->exists('file.txt'));
-		$this->assertTrue($this->adapter->exists('file2.txt'));
-		$this->assertEquals($this->adapter->get_contents('file.txt'), 'abc');
-		$this->assertEquals($this->adapter->get_contents('file.txt'), 'abc');
-		$this->adapter->delete('file.txt');
-		$this->adapter->delete('file2.txt');
-	}
-
-	public function test_create_dir()
-	{
-		$this->adapter->create_dir('path/to/dir');
-		$this->assertTrue($this->adapter->exists('path/to/dir'));
-		$this->adapter->delete_dir('path');
-	}
-
-	public function test_delete_dir()
-	{
-		$this->adapter->create_dir('directory');
-		$this->adapter->put_contents('directory/file.txt', '');
-		$this->assertTrue($this->adapter->exists('directory'));
-		$this->assertTrue($this->adapter->exists('directory/file.txt'));
-		$this->adapter->delete_dir('directory');
-		$this->assertFalse($this->adapter->exists('directory'));
-		$this->assertFalse($this->adapter->exists('directory/file.txt'));
+		$this->assertEquals(file_get_contents('file.txt'), 'abc');
+		$this->assertEquals(file_get_contents('file.txt'), 'abc');
+		unlink('file.txt');
+		unlink('file2.txt');
 	}
 
 	public function test_read_stream()
