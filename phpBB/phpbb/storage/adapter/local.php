d4666e76c4d8e55af34ad085b2643060503895db
Rubén Calvo <rubencm@gmail.com>
2017-06-25 15:08:40 +0200

    [ticket/15253] Use path from adapter
    PHPBB3-15253

--- phpBB/phpbb/storage/adapter/local.php
+++ phpBB/phpbb/storage/adapter/local.php
@@ -45,7 +45,12 @@ class local implements adapter_interface
 	public function __construct(filesystem $filesystem, $phpbb_root_path)
 	{
 		$this->filesystem = $filesystem;
-		$this->phpbb_root_path = $phpbb_root_path;
+		$this->root_path = $root_path.$config[$path_key];
+
+		if(substr($this->root_path, -1, 1) != DIRECTORY_SEPARATOR)
+		{
+			$this->root_path = $this->root_path.DIRECTORY_SEPARATOR;
+		}
 	}
 
 	/**
